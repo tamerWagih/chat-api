bfe57c5dd8a3f28c86580d8b6b06dc1d640531ab
Tamer Wagih <mrtamerwagih@gmail.com>
2021-05-20 01:14:31 +0300

    Initial, Adding server, routes, db, pusher for realtime messages

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Messages = require('./dbMessages');
+const Pusher = require('pusher');
+require('dotenv').config();
+
+//* App config
+const app = express();
+const port = process.env.PORT || 3001;
+
+const pusher = new Pusher({
+  appId: '1206584',
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: 'eu',
+  useTLS: true,
+});
+
+//* Middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+//* DB Config
+const connectionUrl = process.env.DB_URL;
+
+mongoose.connect(connectionUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('DB Connected');
+  const mgsCollection = db.collection('messagecontents');
+  const changeStream = mgsCollection.watch();
+
+  changeStream.on('change', (change) => {
+    console.log(change);
+    if (change.operationType === 'insert') {
+      const messageDetails = change.fullDocument;
+      pusher.trigger('messages', 'inserted', {
+        name: messageDetails.name,
+        message: messageDetails.message,
+      });
+    } else {
+      console.log('Error trigerring Pusher');
+    }
+  });
+});
+
+// * routes
+app.get('/', (req, res) => {
+  res.status(200).send('Api Working');
+});
+
+app.get('/messages/sync', (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post('/messages/new', (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`new message created: \n ${data}`);
+    }
+  });
+});
+
+// * Listening
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
